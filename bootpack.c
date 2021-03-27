7cd5aa552d00fdbfff3fb242e1660b6701074b32
liweiforeveryoung <liweiforeveryoung@gmail.com>
2021-03-27 17:23:37 +0800

    显示画面

--- bootpack.c
+++ bootpack.c
@@ -1,12 +1,17 @@
-////
-//// Created by liwei1 on 2021/3/27.
-////
+//
+// Created by liwei1 on 2021/3/27.
+//
 void io_hlt(void);
 
-//
-void HariMain(void)
-{
-    fin:
-    io_hlt();
-    goto fin;
+void write_mem8(int addr, int data);
+
+void HariMain(void) {
+    int i;
+    // 估计是编译器落后了  for (int i = 0xa0000; i <= 0xaffff; ++i) 死活编译不过
+    for (i = 0xa0000; i <= 0xaffff; ++i) {
+        write_mem8(i, 15); /* MOV BYTE [i],15 */
+    }
+    for (;;) {
+        io_hlt();
+    }
 }
