1320da43810cad76aa671b203c28a50f7ffd9da0
李威1 <liwei1@yuqi.local>
2021-03-27 17:37:06 +0800

    不用 write_mem8，仅仅用指针来展示画面

--- bootpack.c
+++ bootpack.c
@@ -6,10 +6,9 @@ void io_hlt(void);
 void write_mem8(int addr, int data);
 
 void HariMain(void) {
-    int i;
-    // 估计是编译器落后了  for (int i = 0xa0000; i <= 0xaffff; ++i) 死活编译不过
-    for (i = 0xa0000; i <= 0xaffff; ++i) {
-        write_mem8(i, 15); /* MOV BYTE [i],15 */
+    char *p = (char *) 0xa0000;
+    for (; p < (char *) 0xaffff; ++p) {
+        *p = 15;
     }
     for (;;) {
         io_hlt();
