6a0f9e08c625f1985138ec9f6adeff33f7df7c62
liweiforeveryoung <liweiforeveryoung@gmail.com>
2021-03-29 22:10:01 +0800

    初始化 idt 与 gdt

--- bootpack.c
+++ bootpack.c
@@ -55,6 +55,16 @@ void print_mouse(unsigned char *vRam, short xSize, short x0, short y0,
 // 初始化鼠标
 void init_mouse_cursor8(char *mouse, char bc);
 
+// 段描述符
+void load_gdtr(int limit, int addr);
+
+// 中段向量
+void load_idtr(int limit, int addr);
+
+void init_gdt();
+
+void init_idt();
+
 unsigned char *const VRam_Addr_Begin = (unsigned char *const) 0xa0000;
 unsigned char *const VRam_Addr_End = (unsigned char *const) 0xaffff;
 
@@ -66,6 +76,25 @@ struct BootInfo {
     short screenX;      // 分辨率的 x (screen x)
     short screenY;      // 分辨率的 y (screen y)
     unsigned char *vRamAddr;     // 图像缓冲区的开始地址
+};
+
+// 对于段的描述
+struct SEGMENT_DESCRIPTOR {
+    short limit_low;
+    short base_low;
+    char base_mid;
+    char access_right;
+    char limit_high;
+    char base_high;
+};
+
+// 对于中断向量的描述
+struct INTERRUPT_DESCRIPTOR {
+    short offset_low;
+    short selector;
+    char dw_count;
+    char access_right;
+    short offset_high;
 };
 
 struct BootInfo *const Boot_Info_Ptr = (struct BootInfo *const) 0x0ff0;
@@ -89,6 +118,8 @@ void HariMain(void) {
     print_str(Boot_Info_Ptr->vRamAddr, Boot_Info_Ptr->screenX, 8, 8, s, COLOR_BLACK);
     init_mouse_cursor8(mouse, COLOR_WHITE);
     print_mouse(Boot_Info_Ptr->vRamAddr, Boot_Info_Ptr->screenX, 32, 32, 16, 16, mouse);
+    init_gdt();
+    init_idt();
     run();
 }
 
@@ -219,6 +250,52 @@ void init_mouse_cursor8(char *mouse, char bc) {
         }
     }
 }
+
+void set_segment_desc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar) {
+    if (limit > 0xfffff) {
+        ar |= 0x8000; /* G_bit = 1 */
+        limit /= 0x1000;
+    }
+    sd->limit_low = limit & 0xffff;
+    sd->base_low = base & 0xffff;
+    sd->base_mid = (base >> 16) & 0xff;
+    sd->access_right = ar & 0xff;
+    sd->limit_high = ((limit >> 16) & 0x0f) | ((ar >> 8) & 0xf0);
+    sd->base_high = (base >> 24) & 0xff;
+}
+
+// 初始化 global descriptor table（段表）
+void init_gdt() {
+    // gdt 的地址
+    struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) 0x00270000;
+    // init all segment descriptors
+    int i;
+    for (i = 0; i < 8192; ++i) {
+        set_segment_desc(gdt + i, 0, 0, 0);
+    }
+    set_segment_desc(gdt + 1, 0xffffffff, 0x00000000, 0x4092);
+    set_segment_desc(gdt + 2, 0x0007ffff, 0x00280000, 0x409a);
+    load_gdtr(0xffff, (int) 0x00270000);
+}
+
+void set_interrupt_desc(struct INTERRUPT_DESCRIPTOR *id, int offset, int selector, int ar) {
+    id->offset_low = offset & 0xffff;
+    id->selector = selector;
+    id->dw_count = (ar >> 8) & 0xff;
+    id->access_right = ar & 0xff;
+    id->offset_high = (offset >> 16) & 0xffff;
+}
+
+// 初始化中断向量表  interrupt descriptor table
+void init_idt() {
+    struct INTERRUPT_DESCRIPTOR *idt = (struct INTERRUPT_DESCRIPTOR *) 0x0026f800;
+    int i;
+    for (i = 0; i < 256; i++) {
+        set_interrupt_desc(idt + i, 0, 0, 0);
+    }
+    load_idtr(0x7ff, (int) idt);
+}
+
 
 void run() {
     while (1) {
