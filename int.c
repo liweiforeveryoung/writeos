58b341ab91b2be08e97c852cc3e5d101e04a1493
liweiforeveryoung <liweiforeveryoung@gmail.com>
2021-04-03 15:31:24 +0800

    打印键盘输入，一定要用中文输入法输入呀

--- int.c
+++ int.c
@@ -42,11 +42,13 @@ void init_pic(void) {
 
 // handler keyboard event
 void int_handler21(int *esp) {
-    static int count = 0;
     io_out8(PIC0_OCW2, 0x61);
-    int data = io_in8(PORT_KEYDAT); // 想要处理下一次键盘中断有两个条件：1、io_out8 ocw 2、通过 io_in 把数据从端口中读出来；两者缺一不可
-    print_char(Boot_Info_Ptr->vRamAddr, Boot_Info_Ptr->screenX, 16, 16, 'a' + count, COLOR_BLACK);
-    count++;
+    // 这个地方切忌要用 unsigned char 来接收，用 char 的话打印出来的东西会有点奇怪
+    unsigned char input = io_in8(PORT_KEYDAT); // 想要处理下一次键盘中断有两个条件：1、io_out8 ocw 2、通过 io_in 把数据从端口中读出来；两者缺一不可
+    char str[4] = {0};
+    sprintf(str, "%x", input);
+    box_fill8(Boot_Info_Ptr->vRamAddr, Boot_Info_Ptr->screenX, 20, 20, 200, 200, COLOR_WHITE);
+    print_str(Boot_Info_Ptr->vRamAddr, Boot_Info_Ptr->screenX, 20, 20, str, COLOR_BLACK);
 }
 
 // handler mouse event
