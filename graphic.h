3f1da734a48a34fa1998031510201b18afa0492a
李威1 <liwei1@yuqi.local>
2021-04-13 22:05:49 +0800

    一个有模有样的窗口

--- graphic.h
+++ graphic.h
@@ -11,14 +11,14 @@ void init_mouse_cursor8(char *mouse, char bc);
 void print_mouse(unsigned char *vRam, short xSize, short x0, short y0,
                  short mouseWidth, short mouseHeight, const char *mouse);
 
-void print_str(unsigned char *vRam, short xSize, short x0, short y0, const char *string, char color);
+void print_str(char *vRam, short xSize, short x0, short y0, const char *string, char color);
 
 // 打印字体
-void print_raw_char(unsigned char *vRam, short xSize, short x0, short y0, const char *c, char color);
+void print_raw_char(char *vRam, short xSize, short x0, short y0, const char *c, char color);
 
-void print_char(unsigned char *vRam, short xSize, short x0, short y0, char font, char color);
+void print_char(char *vRam, short xSize, short x0, short y0, const char font, char color);
 
-void box_fill8(unsigned char *vRam, int xSize, int x0, int y0, int x1, int y1, unsigned char c);
+void box_fill8(char *vRam, short xSize, int x0, int y0, int x1, int y1, unsigned char c);
 
 // 设置调色板
 void init_palette(void);
