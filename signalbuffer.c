bcc0d6c2a2b93fa3f08184872431b3e5f357bc73
李威1 <liwei1@yuqi.local>
2021-04-30 15:53:44 +0800

    将 KeyBuffer 重命名为 SignalBuffer，因为这个 buffer 并不仅仅存储键盘信号了，它还存储定时器信号。

--- signalbuffer.c
+++ signalbuffer.c
@@ -1,9 +1,9 @@
 //
 // Created by liwei1 on 2021/4/3.
 //
-#include "keybuffer.h"
+#include "signalbuffer.h"
 
-void init_key_buffer(struct KeyBuffer *buffer) {
+void init_signal_buffer(struct SignalBuffer *buffer) {
     buffer->cur_index = 0;
     buffer->end_index = 0;
 }
@@ -14,33 +14,33 @@ static int next_index(int length, int cur_index) {
 }
 
 // 判斷 buffer 是否滿了
-bool buffer_is_full(struct KeyBuffer *buffer) {
-    return next_index(KeyBufferLength, buffer->end_index) == buffer->cur_index;
+bool buffer_is_full(struct SignalBuffer *buffer) {
+    return next_index(SignalBufferLength, buffer->end_index) == buffer->cur_index;
 }
 
 // buffer 是否是空的
-bool buffer_is_empty(struct KeyBuffer *buffer) {
+bool buffer_is_empty(struct SignalBuffer *buffer) {
     return buffer->cur_index == buffer->end_index;
 }
 
 // 成功则返回 true，失败则返回 false
-bool write_data_into_buffer(struct KeyBuffer *buffer, unsigned char datum, unsigned char type) {
+bool write_data_into_buffer(struct SignalBuffer *buffer, unsigned char datum, unsigned char type) {
     if (buffer_is_full(buffer)) {
         return false;
     }
     buffer->data[buffer->end_index] = datum;
     buffer->type[buffer->end_index] = type;
-    buffer->end_index = next_index(KeyBufferLength, buffer->end_index);
+    buffer->end_index = next_index(SignalBufferLength, buffer->end_index);
     return true;
 }
 
-bool read_data_from_buffer(struct KeyBuffer *buffer, unsigned char *datum, unsigned char *type) {
+bool read_data_from_buffer(struct SignalBuffer *buffer, unsigned char *datum, unsigned char *type) {
     if (buffer_is_empty(buffer)) {
         return false;
     }
     *datum = buffer->data[buffer->cur_index];
     *type = buffer->type[buffer->cur_index];
-    buffer->cur_index = next_index(KeyBufferLength, buffer->cur_index);
+    buffer->cur_index = next_index(SignalBufferLength, buffer->cur_index);
     return true;
 }
 
